f8902e532ac9a8ddd8904ede34c408df77df237d
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 16:13:10 +0530

    fixing components

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import "/styles/css/plugins/fontawesome-5.css";
-import "/styles/css/plugins/swiper.css";
-import "/styles/css/plugins/aos.css";
-import "/styles/css/plugins/unicons.css";
-import "/styles/css/plugins/metismenu.css";
-import "/styles/css/plugins/hover-revel.css";
-import "/styles/css/plugins/timepickers.min.css";
-import "/styles/css/vendor/bootstrap.min.css";
+import "/styles/css/bootstrap.min.css";
+import "/styles/css/flaticon.css";
+import "/styles/css/remixicon.css";
+import "/styles/css/owl.carousel.min.css";
+import "/styles/css/owl-theme-default-min.css";
+import "/styles/css/odometer.min.css";
+import "/styles/css/fancybox.css";
+import "/styles/css/aos.css";
 import "/styles/css/style.css";
-
+import "/styles/css/responsive.css";
+import "/styles/css/dark-theme.css";
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
