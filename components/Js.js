1bbc01bac716687ffb4a9710b53731141453da0d
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 16:49:01 +0530

    Update Js.js

--- components/Js.js
+++ components/Js.js
@@ -1,10 +1,19 @@
 export default function Js() {
   return (
     <>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/jquery.min.js"></script>
-
-     
+        <script src="https://www.ashokseedplant.com/js/jquery.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/bootstrap.bundle.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/form-validator.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/contact-form-script.js"></script>
+        <script src="https://www.ashokseedplant.com/js/aos.js"></script>
+        <script src="https://www.ashokseedplant.com/js/owl.carousel.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/owl-thumb.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/odometer.js"></script>
+        <script src="https://www.ashokseedplant.com/js/circle-progressbar.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/fancybox.js"></script>
+        <script src="https://www.ashokseedplant.com/js/jquery.appear.js"></script>
+        <script src="https://www.ashokseedplant.com/js/tweenmax.min.js"></script>
+        <script src="https://www.ashokseedplant.com/js/main.js"></script>
     </>
   )
 }
