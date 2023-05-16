6b9e0a120266d8b638176044e545cc44655cbfef
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 16:16:24 +0530

    Update Js.js

--- components/Js.js
+++ components/Js.js
@@ -4,42 +4,7 @@ export default function Js() {
 
           <script src="https://www.waveearthinfratech.com/js/plugins/jquery.min.js"></script>
 
-          <script src="https://www.waveearthinfratech.com/js/vendor/jqueryui.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/counter-up.js"></script>
-          <script src="https://www.waveearthinfratech.com/js/plugins/swiper.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/vendor/twinmax.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/vendor/split-text.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/text-plugins.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/metismenu.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/vendor/waypoint.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/vendor/waw.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/aos.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/jquery-ui.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/jquery-timepicker.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/vendor/sal.min.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/bootstrap.min.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/jquery-slideNav.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/hover-revel.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/contact-form.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/main.js"></script>
-
-          <script src="https://www.waveearthinfratech.com/js/plugins/swip-img.js"></script>
+     
     </>
   )
 }
