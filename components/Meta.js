9bf5a1a562d15c15b094cdc081196225ae2aae8b
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 16:35:44 +0530

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -6,7 +6,7 @@ export default function Meta() {
                 <meta charSet="utf-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link href="/images/fav.png" rel="shortcut icon" />
+                <link href="/img/favicon.png" type="image/png" rel="shortcut icon" />
             </Head>
         </>
     )
