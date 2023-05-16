1dc93e6934b63eed3bb5cbb38ba95d687d4da312
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 16:52:10 +0530

    fixed home slider

--- pages/404.js
+++ pages/404.js
@@ -18,7 +18,7 @@ export default function Home() {
             <div className="page-wrapper">
                 <Header />
 
-                <Sidebar />
+
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,72 @@ export default function Home() {
             <div className="page-wrapper">
             <Header />
 
-            <Sidebar />
+                <section className="hero-wrap style2">
+                    <div className="hero-slider-one owl-carousel" data-slider-id="1">
+                        <div className="hero-slide-item hero-slide-one bg-f">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-lg-6  col-md-9">
+                                        <div className="hero-content" data-speed="0.10" data-revert="true">
+                                            <span>We'll Save Our Planet</span>
+                                            <h1>Lets Make Our Earth Green and Clean</h1>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollic itudin
+                                                consectetur netus dui, ultrices or lectus ac egestas. Vivamus tellus
+                                                vestibulum aliquet arcu.</p>
+                                            <a href="register.html" className="btn style2">Join With us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="hero-slide-item hero-slide-two bg-f">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-lg-6 col-md-9">
+                                        <div className="hero-content" data-speed="0.10" data-revert="true">
+                                            <span>Save Our Soil</span>
+                                            <h1>Stop Using Plastic And Make Our land More Fertile</h1>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin
+                                                consectetur netus dui, ultrices or lectus ac egestas. Vivamus tellus
+                                                vestibulum aliquet arcu a duis. </p>
+                                            <a href="register.html" className="btn style2">Join With us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="hero-slide-item hero-slide-three bg-f">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-lg-6  col-md-10">
+                                        <div className="hero-content" data-speed="0.10" data-revert="true">
+                                            <span>We'll Save Our Planet</span>
+                                            <h1>Lets Start Using Green Energy For A Better Planet</h1>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin
+                                                consectetur netus dui, ultrices or lectus ac egestas. Vivamus tellus
+                                                vestibulum aliquet arcu a duis. </p>
+                                            <a href="register.html" className="btn style2">Join With us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hero-slider-thumbs owl-thumbs" data-slider-id="1">
+                        <button className="owl-thumb-item">
+                            <img src="/img/hero/hero-thumb-1.jpg" alt="Images"/>
+                        </button>
+                        <button className="owl-thumb-item">
+                            <img src="/img/hero/hero-thumb-2.jpg" alt="Images"/>
+                        </button>
+                        <button className="owl-thumb-item">
+                            <img src="/img/hero/hero-thumb-3.jpg" alt="Images"/>
+                        </button>
+                    </div>
+                </section>
 
 
-
-            <Footer />
+                <Footer />
             </div>
             <Js />
         </>
