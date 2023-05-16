8060c767603191f1d95f107b2df87d73bfeb0a82
Shiv Singh <shivsingh7150@hotmail.com>
2023-05-16 17:03:33 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -18,7 +18,27 @@ export default function Home() {
             <div className="page-wrapper">
                 <Header />
 
+                <div className="content-wrapper">
 
+                    <!-- Error  Section start -->
+                    <div className="error-wrap ptb-100">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-8 offset-lg-2">
+                                    <div className="error-content">
+                                        <img src="/img/404.png" alt="Iamge"/>
+                                            <h2>Oops! Page Not Found</h2>
+                                            <p>The page you are looking for might have been removed had its name changed
+                                                or is temporarily unavailable.</p>
+                                            <a href="index.html" className="btn style1">Back To Home</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Error  Section end -->
+
+                </div>
 
 
 
