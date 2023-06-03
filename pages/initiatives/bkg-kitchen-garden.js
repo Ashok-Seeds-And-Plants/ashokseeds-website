de9972bfff94a458ca2adb5cb4e78b9eb2e16ded
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-03 10:51:58 +0530

    Update bkg-kitchen-garden.js

--- pages/initiatives/bkg-kitchen-garden.js
+++ pages/initiatives/bkg-kitchen-garden.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Meta from '@components/Meta'
 import Header from '@components/Header'
-import Sidebar from '@components/Sidebar'
 import Footer from '@components/Footer'
 import Js from '@components/Js'
 
@@ -11,7 +10,7 @@ export default function Home() {
         <>
             <Meta />
             <Head>
-                <title>404 Page Not Found | Ashok Seeds and Plants</title>
+                <title>BKG Kitchen Garden | Ashok Seeds and Plants</title>
                 <meta name="description" content="" />
 
             </Head>
