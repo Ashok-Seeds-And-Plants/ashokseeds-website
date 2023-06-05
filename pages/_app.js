3b33fafc40874aacdd0744d5db1b887e66432edc
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-05 13:39:32 +0530

    css

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,4 @@
-import "/styles/css/bootstrap.min.css";
-import "/styles/css/flaticon.css";
-import "/styles/css/remixicon.css";
-import "/styles/css/owl.carousel.min.css";
-import "/styles/css/owl-theme-default-min.css";
-import "/styles/css/odometer.min.css";
-import "/styles/css/fancybox.css";
-import "/styles/css/aos.css";
-import "/styles/css/style.css";
-import "/styles/css/responsive.css";
-import "/styles/css/dark-theme.css";
-import "/styles/css/home.css";
+
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
