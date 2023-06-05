4cd8a5a2f8629caae4becebe363943d71c1fc87f
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-05 13:42:10 +0530

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -7,17 +7,17 @@ export default function Meta() {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link href="/img/favicon.png" type="image/png" rel="shortcut icon" />
-                <link rel="stylesheet" href="/styles/css/bootstrap.min.css" />
-                <link rel="stylesheet" href="/styles/css/flaticon.css" />
-                <link rel="stylesheet" href="/styles/css/remixicon.css" />
-                <link rel="stylesheet" href="/styles/css/owl.carousel.min.css" />
-                <link rel="stylesheet" href="/styles/css/owl-theme-default-min.css" />
-                <link rel="stylesheet" href="/styles/css/odometer.min.css" />
-                <link rel="stylesheet" href="/styles/css/fancybox.css" />
-                <link rel="stylesheet" href="/styles/css/aos.css" />
-                <link rel="stylesheet" href="/styles/css/style.css" />
-                <link rel="stylesheet" href="/styles/css/responsive.css" />
-                <link rel="stylesheet" href="/styles/css/dark-theme.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/flaticon.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/remixicon.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/owl.carousel.min.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/owl-theme-default-min.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/odometer.min.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/fancybox.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/aos.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/style.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/responsive.css" />
+                <link rel="stylesheet" href="https://www.ashokseedplant.com/styles/css/dark-theme.css" />
             </Head>
         </>
     )
