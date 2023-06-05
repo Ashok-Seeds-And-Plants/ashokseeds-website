694dfd5fe1acb4aaadc7dbdf033f3df3cfac4492
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-05 11:57:47 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -299,6 +299,28 @@
         }
     });
 
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
     //sticky Header
     var wind = $(window);
     var sticky = $('.header-wrap');
