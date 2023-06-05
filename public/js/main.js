4709f13c740c8ab738d779b81d2877c24ed2ca1c
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-05 13:27:59 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -299,11 +299,14 @@
         }
     });
 
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        //direction: 'vertical',
+    const swiper = new Swiper('.home-slider', {
+
         loop: true,
-        //slidesPerView: 3,
+
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
 
         // If we need pagination
         pagination: {
