d24d35b95fa9f18288b6f6b3c5c552eddaf341e6
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-08 14:58:02 +0530

    Update Posts.js

--- components/Posts.js
+++ components/Posts.js
@@ -23,83 +23,24 @@ const Posts = ({ posts }) => {
 
                         console.log(post);
                         const title = delve(post, "attributes.title");
-                        console.log(title);
+                        //console.log(title);
                         return (
-                            <div>{delve(post, "attributes.title")}</div>
+                            <div className="blog-card style3">
+                                <div className="blog-img">
+                                    <img src="/img/blog/blog-3.jpg" alt="Image"/>
+                                    <a href="#" className="blog-date"><span>25</span> Jul, 2022</a>
+                                </div>
+                                <div className="blog-info">
+                                    <h3><a href="#">{title}</a></h3>
+                                    <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
+                                        ame ultrices lectus dolor sit amet egestas.</p>
+                                    <a href="#" className="link style1">Read More <i
+                                        className="flaticon-right-arrow"></i></a>
+                                </div>
+                            </div>
                         )
                     })}
 
-
-                    <div className="blog-card style3">
-                        <div className="blog-img">
-                            <img src="/img/blog/blog-3.jpg" alt="Image"/>
-                            <a href="#" className="blog-date"><span>25</span> Jul, 2022</a>
-                        </div>
-                        <div className="blog-info">
-                            <h3><a href="#">Changes In Landscapes And Wildlife
-                                Community</a></h3>
-                            <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
-                                ame ultrices lectus dolor sit amet egestas.</p>
-                            <a href="#" className="link style1">Read More <i
-                                className="flaticon-right-arrow"></i></a>
-                        </div>
-                    </div>
-                    <div className="blog-card style3">
-                        <div className="blog-img">
-                            <img src="/img/blog/blog-1.jpg" alt="Image"/>
-                            <a href="#" className="blog-date"><span>18</span> Jul, 2022</a>
-                        </div>
-                        <div className="blog-info">
-                            <h3><a href="#">Let’s Take Care of Nature for The
-                                Sake of The Earth</a></h3>
-                            <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
-                                ame ultrices lectus dolor sit amet egestas.</p>
-                            <a href="#" className="link style1">Read More <i
-                                className="flaticon-right-arrow"></i></a>
-                        </div>
-                    </div>
-                    <div className="blog-card style3">
-                        <div className="blog-img">
-                            <img src="/img/blog/blog-2.jpg" alt="Image"/>
-                            <a href="#" className="blog-date"><span>10</span> Jul, 2022</a>
-                        </div>
-                        <div className="blog-info">
-                            <h3><a href="#">Increasing Risk Of Storms,
-                                Droughts and Floods</a></h3>
-                            <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
-                                ame ultrices lectus dolor sit amet egestas.</p>
-                            <a href="#" className="link style1">Read More <i
-                                className="flaticon-right-arrow"></i></a>
-                        </div>
-                    </div>
-                    <div className="blog-card style3">
-                        <div className="blog-img">
-                            <img src="/img/blog/blog-4.jpg" alt="Image"/>
-                            <a href="#" className="blog-date"><span>17</span> May, 2022</a>
-                        </div>
-                        <div className="blog-info">
-                            <h3><a href="#">Seven Things Nobody Told You About
-                                Water Pollution</a></h3>
-                            <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
-                                ame ultrices lectus dolor sit amet egestas.</p>
-                            <a href="#" className="link style1">Read More <i
-                                className="flaticon-right-arrow"></i></a>
-                        </div>
-                    </div>
-                    <div className="blog-card style3">
-                        <div className="blog-img">
-                            <img src="/img/blog/blog-5.jpg" alt="Image"/>
-                            <a href="#" className="blog-date"><span>12</span> May, 2022</a>
-                        </div>
-                        <div className="blog-info">
-                            <h3><a href="#">Solar Panels Will Be Great Source
-                                Of Green Energy</a></h3>
-                            <p>Lorem ipsum dolor sit amet cons ectadic elite soli tudin consec tetur netusdu
-                                ame ultrices lectus dolor sit amet egestas.</p>
-                            <a href="#" className="link style1">Read More <i
-                                className="flaticon-right-arrow"></i></a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
