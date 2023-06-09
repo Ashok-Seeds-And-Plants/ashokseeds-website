6f8658860964a6ba1dcd48913834c772c851e2de
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-09 14:54:24 +0530

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,10 +4,11 @@ import Meta from '@components/Meta'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Js from '@components/Js'
-
+import ReactMarkdown from "react-markdown";
 import { fetchAPI } from "../../lib/api"
 import delve from 'dlv'
 import { parseISO, format } from 'date-fns'
+
 
 const Blog = ({ post, categories }) => {
     const title = delve(post, "attributes.title");
@@ -55,76 +56,7 @@ const Blog = ({ post, categories }) => {
                                             <li><i className="ri-calendar-todo-line"></i>{format(date, 'd LLL yyyy')}</li>
                                         </ul>
                                         <div className="post-para">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
-                                                tempor incididunt labore et dolore sitor magna aliqua. Quis ipsum
-                                                suspendisse ultrices <strong>gravida</strong>. Risus commodo viverra
-                                                manas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet,
-                                                consectetur adipiscing elit.</p>
-                                            <p>No sea takimata sanctus est Lorem <a href="index.html">Ipsum</a> dolor
-                                                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                                                diam nonumy eirmod tempor invidunt ut labore consect etur adicing elit.
-                                            </p>
-                                            <blockquote className="wp-block-quote">
-                                                <p>“We denounce with righteous indignation and dislike men who are so
-                                                    beguidemralized by the charms of pleasure of the moment, so blinded
-                                                    by desire, that they cannot”</p>
-                                            </blockquote>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
-                                                tempor incididunt labore et dolore sitor magna aliqua. Quis ipsum
-                                                suspendisse ultrices gravida. Risus commodo viverra manas accumsan lacus
-                                                vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing
-                                                elit.</p>
-                                            <h3>Effect Of Defroestation On Our Environment</h3>
-                                            <ol>
-                                                <li>Lacus sed viverra tellus in hac habitasse platea dictumst.</li>
-                                                <li>Gravida neque convallis a <strong>cras</strong> semper auctor neque
-                                                    vitae.
-                                                </li>
-                                                <li>Lacus sed turpis tincidunt id aliquet risus feugiat in.</li>
-                                                <li>Risus commodo viverra manas accumsan lacus vel facilisis</li>
-                                            </ol>
-                                            <div className="row">
-                                                <div className="col-md-6">
-                                                    <a className="post-img" data-fancybox="gallery"
-                                                       href="/img/blog/single-blog-2.jpg">
-                                                        <img src="/img/blog/single-blog-2.jpg" alt="Image"/>
-                                                    </a>
-                                                </div>
-                                                <div className="col-md-6">
-                                                    <a className="post-img" data-fancybox="gallery"
-                                                       href="/img/blog/single-blog-3.jpg">
-                                                        <img src="/img/blog/single-blog-3.jpg" alt="Image"/>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <h3>What Health Issues Arise For Climate Change</h3>
-                                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-                                                eirmod tempor invidunt ut labore dolore magna aliquyam erat, sed diam
-                                                voluptua at vero amet dolor sit consect.Consectetur adipisicing elit.
-                                                Laboriosam in culpa dolorem. Eos sint aut suscipit.</p>
-                                            <ul className="content-feature-list style1 list-style mt-15">
-                                                <li><i className="ri-checkbox-circle-line"></i>
-                                                    Lorem ipsum dolor, sit amet.
-                                                </li>
-                                                <li><i className="ri-checkbox-circle-line"></i>
-                                                    Amet consectetur adipisicing elit Officia.
-                                                </li>
-                                                <li><i className="ri-checkbox-circle-line"></i>
-                                                    Aquaerat ipsa quis possimus.
-                                                </li>
-                                                <li><i className="ri-checkbox-circle-line"></i>
-                                                    Lorem aquaerat ipsa quis possimus.
-                                                </li>
-                                                <li><i className="ri-checkbox-circle-line"></i>
-                                                    Consectetur Amet adipisicing elit Officia.
-                                                </li>
-                                            </ul>
-                                            <p>Lorem ipsum dolor sit amet,
-                                                consectetur <strong>adipisicing</strong> elit, sed do eiusmod tempor
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat. Duis aute irure dolor in reprehenderit in <a
-                                                    href="index.html">sed</a> uia non numquam eius modi tempora incidunt
-                                                ut labore dolor.</p>
+                                            <ReactMarkdown source={post.attributes.Content} escapeHtml={false} />
                                         </div>
                                     </article>
                                     <div className="post-meta-option">
@@ -173,9 +105,7 @@ const Blog = ({ post, categories }) => {
                                         </div>
                                         <div className="post-author-info">
                                             <h4>Posted by<a href="posts-by-author.html"> {username}</a></h4>
-                                            <p>Claritas est etiam amet sinicus, qui sequitur lorem ipsum semet coui
-                                                lectorum. Lorem ipsum dolor voluptatem corporis blanditiis sadipscing
-                                                elitr sed diam nonumy eirmod amet sit lorem.</p>
+                                            <ReactMarkdown source={post.attributes.user.data.attributes.about} escapeHtml={false} />
                                             <ul className="social-profile list-style style3">
                                                 <li>
                                                     <a href="https://facebook.com">
