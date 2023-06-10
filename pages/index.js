93c46f2e6c39af252fc8d2f8480d3bad093470a0
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-10 16:44:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -193,6 +193,33 @@ const Home = ({ posts, galleries, portfolios }) => {
                     </div>
                 </div>
 
+                <section className="project-wrap ptb-100 bg-sand">
+                    <div className="container">
+                        <div className="section-title style3 text-center mb-40">
+                            <span>Journey <span className="bl-text">So for</span></span>
+                            <h2>Our Completed Projects</h2>
+                        </div>
+                        <div className="project-slider-three owl-carousel">
+
+                            <div className="project-card style4" data-aos="fade-right" data-aos-duration="1200"
+                                 data-aos-delay="600">
+                                <div className="project-img">
+                                    <img src="/img/project/project-22.jpg" alt="Image"/>
+                                        <button className="like-btn" type="button"><i className="ri-heart-fill"></i>
+                                        </button>
+                                </div>
+                                <div className="project-info">
+                                    <h3><a href="project-details.html">Reduce Air Pollution
+                                    </a></h3>
+                                    <p>Lorem ipsum dolor sit amet cons ecte adipiscin elit.</p>
+                                    
+                                    <a href="#" className="link style1">See More<i
+                                        className="flaticon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
                 <section className="team-wrap ptb-100 bg-sand">
                 <img src="/img/shape-11.png" alt="Image" className="team-shape-one"/>
