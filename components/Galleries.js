aa3242a67476fd1fcba943e29c2006c36f1f0e31
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-10 16:27:07 +0530

    Update Galleries.js

--- components/Galleries.js
+++ components/Galleries.js
@@ -3,26 +3,18 @@ import delve from 'dlv'
 import { parseISO, format } from 'date-fns'
 
 const Galleries = ({ galleries }) => {
-    //const leftArticlesCount = Math.ceil(articles.length / 5)
-    //const leftArticles = articles.slice(0, leftArticlesCount)
-    //const rightArticles = articles.slice(leftArticlesCount, articles.length)
-
-    // console.log(posts);
 
     return (
 
         <div className="team-slider-two owl-carousel">
             {galleries.map((gallery, i) => {
 
-                //console.log(post);
-               // const title = delve(post, "attributes.title");
-                //const slug = delve(post, "attributes.slug");
-               // const cover = delve(post, "attributes.cover.data.attributes.formats.medium.url");
-                //const date = parseISO(delve(post, "attributes.publishedAt"));
-                // console.log(date);
+               const title = delve(gallery, "attributes.title");
+               const image = delve(gallery, "attributes.image.data.attributes.formats.medium.url");
+
                 return (
             <div className="team-card style2" data-aos="fade-right" data-aos-duration="1200" data-aos-delay="200">
-                <img src="/img/team/team-1.jpg" alt="Image"/>
+                <img src={`${image}`} alt={`${title}`}/>
             </div>
                 )
             })}
