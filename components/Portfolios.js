710d461265c367687b3e595964120e9d06866278
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-12 12:15:41 +0530

    Update Portfolios.js

--- components/Portfolios.js
+++ components/Portfolios.js
@@ -19,19 +19,20 @@ const Portfolios = ({ portfolios }) => {
 
                         const title = delve(portfolio, "attributes.title");
                         const content = delve(portfolio, "attributes.content");
-                        //const image = delve(portfolio, "attributes.image.data.attributes.formats.medium.url");
+                        const slug = delve(portfolio, "attributes.slug");
+                        const cover = delve(portfolio, "attributes.cover.data.attributes.formats.medium.url");
 
                         return (
                     <div className="project-card style4" data-aos="fade-right" data-aos-duration="1200"
                          data-aos-delay="600">
                         <div className="project-img">
-                            <img src="/img/project/project-22.jpg" alt="Image"/>
+                            <img src={`${cover}`} alt={`${title}`}/>
                         </div>
                         <div className="project-info">
-                            <h3><a href="#">{title}</a></h3>
+                            <h3><a href={`/project/${slug}`}>{title}</a></h3>
                             <ReactMarkdown children={content} />
 
-                            <a href="#" className="link style1">See More<i
+                            <a href={`/project/${slug}`} className="link style1">See More<i
                                 className="flaticon-right-arrow"></i></a>
                         </div>
                     </div>
