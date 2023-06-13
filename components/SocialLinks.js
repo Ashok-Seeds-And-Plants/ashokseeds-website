1cc21794c104828540e556e41630481959f72d6a
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-13 15:38:38 +0530

    Update SocialLinks.js

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -1,23 +1,23 @@
 const SocialLinks = ({ style }) => {
-
+const styleName = style;
     return (
-<ul className="social-profile {style} list-style">
-    <li>
-        <a href="https://facebook.com">
-            <i className="ri-facebook-fill"></i>
-        </a>
-    </li>
-    <li>
-        <a href="https://twitter.com">
-            <i className="ri-twitter-fill"></i>
-        </a>
-    </li>
-    <li>
-        <a href="https://instagram.com">
-            <i className="ri-instagram-line"></i>
-        </a>
-    </li>
-</ul>
+        <ul className="social-profile {styleName} list-style">
+            <li>
+                <a href="https://facebook.com">
+                    <i className="ri-facebook-fill"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com">
+                    <i className="ri-twitter-fill"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://instagram.com">
+                    <i className="ri-instagram-line"></i>
+                </a>
+            </li>
+        </ul>
     )
 }
 
