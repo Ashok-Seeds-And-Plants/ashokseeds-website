94acef95e32082714685c1c7ad365b472bd8a97b
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-15 23:36:21 +0530

    Update miyawaki-forest.js

--- pages/initiatives/miyawaki-forest.js
+++ pages/initiatives/miyawaki-forest.js
@@ -60,6 +60,24 @@ export default function Home() {
                             </div>
                         </div>
                     </section>
+                    <div className="promo-video style2 bg-f ptb-100">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
+                                    <div className="content-title style4 text-center mb-40">
+                                        <span>Miyawaki Method of Plantation <span className="bl-text">Our Video</span></span>
+                                        <h2>The best method to grow an urban forest within a short span of 10-20 years.</h2>
+                                    </div>
+                                    <a className="play-now" data-fancybox=""
+                                       href="https://www.youtube.com/watch?v=l5jtg2q1gnU">
+                                        <i className="ri-play-fill"></i>
+                                        <span className="ripple"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
 
                     <section className="cta-wrap style1 bg-f ptb-100">
                         <img src="/img/bg-shape-5.png" alt="Image" className="cta-shape-two"/>
