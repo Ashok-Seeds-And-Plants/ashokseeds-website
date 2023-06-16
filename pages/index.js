c9013d8aa621ce795c5eb1a49406a8db1fc7b28a
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-16 14:22:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -253,7 +253,7 @@ export async function getStaticProps() {
     const [postsRes, galleriesRes, portfoliosRes] = await Promise.all([
         fetchAPI("/posts", {
             filters: {
-                sort: ['id:desc']
+                sort: ['id:asc']
             },
             pagination: {
                 start: 0,
@@ -263,7 +263,7 @@ export async function getStaticProps() {
         }),
         fetchAPI("/galleries", {
             filters: {
-                sort: ['id:desc']
+                sort: ['id:asc']
             },
             pagination: {
                 start: 0,
@@ -273,7 +273,7 @@ export async function getStaticProps() {
         }),
         fetchAPI("/portfolios", {
             filters: {
-                sort: ['id:desc']
+                sort: ['id:asc']
             },
             pagination: {
                 start: 0,
