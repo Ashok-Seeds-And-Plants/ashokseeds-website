8b98978530ac097fad2cdca4e2b13088f150df89
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-17 18:12:12 +0530

    Update [slug].js

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -16,7 +16,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import "./styles.css";
+//import "./styles.css";
 
 // import required modules
 import { Pagination } from "swiper";
