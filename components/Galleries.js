4a675b3dc7ccb9527c4f36bad8c4cefafb48719f
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-19 17:05:24 +0530

    Update Galleries.js

--- components/Galleries.js
+++ components/Galleries.js
@@ -30,6 +30,24 @@ const Galleries = ({ galleries }) => {
                 disableOnInteraction: false,
             }}
             navigation={true}
+            breakpoints={{
+                "@0.00": {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                },
+                "@0.75": {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                "@1.00": {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                },
+                "@1.50": {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                },
+            }}
             modules={[Autoplay, Pagination, Navigation]}
             className="mySwiper"
         >
