e480e8e331d9fcf5ecadce47c1caf522da7969b2
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-21 14:24:49 +0530

    Update index.js

--- pages/about/gallery/index.js
+++ pages/about/gallery/index.js
@@ -25,66 +25,46 @@ const Gallery = ({ galleries, portfolios }) => {
                     <div className="breadcrumb-wrap bg-f br-1">
                         <div className="container">
                             <div className="breadcrumb-title">
-                                <h2>About Us</h2>
+                                <h2>Gallery</h2>
                                 <ul className="breadcrumb-menu list-style">
                                     <li><a href="https://www.ashokseedplant.com/">Home </a></li>
-                                    <li>About Us</li>
+                                    <li>About</li>
                                     <li>Gallery</li>
                                 </ul>
                             </div>
                         </div>
                     </div>
 
-                    <section className="about-wrap style2 ptb-100">
-                        <img src="/img/about/about-shape-1.png" alt="Image"
-                             className="about-shape-one moveHorizontal" />
+                    <section className="project-details-wrap ptb-100">
                         <div className="container">
-                            <div className="row align-items-center gx-5">
-                                <div className="col-lg-6" data-aos="fade-right" data-aos-duration="1200"
-                                     data-aos-delay="200">
-                                    <div className="about-img-wrap">
-                                        <img src="/img/about/about-img-2.png" alt="Image" className="bounce"/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6" data-aos="fade-left" data-aos-duration="1200"
-                                     data-aos-delay="200">
-                                    <div className="about-content">
-                                        <div className="content-title style3">
-                                            <span>A Little Introduction <span className="bl-text">About Us</span></span>
-                                            <h2>Keeping our Earth safe from climate change.</h2>
-                                            <p>Ashok Seeds and Plants in India are working towards achieving the Sustainable Development Goals (SDGs) through tree plantation, urban biodiversity conservation, water body restoration, sustainable solutions, and environmental research and awareness. We are a rare blend of traditional wisdom and modern and innovative technologies to help businesses achieve their net-zero goal.</p>
+                            <div className="row justify-content-center">
+                                <div className="col-xl-4 col-lg-6 col-md-6">
+                                    <div className="project-card style1">
+                                        <div className="project-img">
+                                            <img src="/img/project/project-1.jpg" alt="Image"/>
                                         </div>
-                                        <ul className="content-feature-list list-style">
-                                            <li><i className="ri-checkbox-circle-line"></i><Link href={'/initiatives/tree-plantation/'}>Tree Plantation</Link></li>
-                                            <li><i className="ri-checkbox-circle-line"></i><Link href={'/initiatives/miyawaki-forest/'}>Miyawaki Forest</Link></li>
-                                            <li><i className="ri-checkbox-circle-line"></i><Link href={'/initiatives/bkg-kitchen-garden/'}>BKG / Kitchen Garden</Link></li>
-                                            <li><i className="ri-checkbox-circle-line"></i>Agroforestry (<Link href={'/initiatives/agroforestry/strawberry/'}>Strawberry</Link>, <Link href={'/initiatives/agroforestry/apple/'}>Apple</Link>, <Link href={'/initiatives/agroforestry/amrood-guava/'}>Amrood (Guava)</Link>, <Link href={'/initiatives/agroforestry/amla-indian-gooseberry/'}>Amla (Indian Gooseberry)</Link>)</li>
-                                        </ul>
+                                        <div className="project-info">
+                                            <img src="/img/shape-1.png" alt="Image" className="project-shape"/>
+                                                <h3>Stop Cutting Down Trees</h3>
+                                            <ul>
+                                            <li>Category</li>
+                                            <li>Category 1</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
+
                             </div>
+                            <ul className="page-nav list-style">
+                                <li><a href="#"><i className="flaticon-left-arrow"></i></a></li>
+                                <li><a className="active" href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#"><i className="flaticon-right-arrow"></i></a></li>
+                            </ul>
                         </div>
                     </section>
 
-                    <section className="cta-wrap style1 bg-f ptb-100">
-                        <img src="/img/bg-shape-5.png" alt="Image" className="cta-shape-two"/>
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2">
-                                    <div className="content-title style1 text-center mb-40" data-aos="fade-up"
-                                         data-aos-duration="1200" data-aos-delay="200">
-                                        <span>What you can do<img src="/img/section-shape.png" alt="Image"/></span>
-                                        <h3>All of our workshops, tree plantings, presentations, outreach programs and educational activities are ABSOLUTELY FREE for government schools, colleges and housing companies.</h3>
-                                    </div>
-                                    <div className="cta-btn" data-aos="fade-up" data-aos-duration="1200"
-                                         data-aos-delay="300">
-                                        <a href="/donate-now/" className="btn style1">Get Started now</a>
-                                        <a href="/get-involved/volunteer/" className="btn style2">Join Our Community</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
 
                 </div>
 
