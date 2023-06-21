ebb4e8fa3bc80a98e099f7615eef71c71d3b2e58
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-21 15:47:40 +0530

    Update index.js

--- pages/about/gallery/index.js
+++ pages/about/gallery/index.js
@@ -25,11 +25,13 @@ const Gallery = ({ galleries, portfolios }) => {
     if (total <= PerPage)
     {
         pages = 0;
-    }else{
+    }else if(total%PerPage == 0){
         pages = pagination.total/pagination.limit;
+    }else{
+        pages = pagination.total/pagination.limit + 1;
     }
 
-
+console.log(pages);
 
 
     const PaginationData = index => {
