1edb5eba0caae6d26a2642e9ab826d7590ab1c68
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-22 10:16:00 +0530

    Update [slug].js

--- pages/about/[slug].js
+++ pages/about/[slug].js
@@ -69,7 +69,7 @@ export async function getStaticPaths() {
     return {
         paths: Pages.map((page) => ({
             params: {
-                slug: +page.id,
+                slug: page.id,
             },
         })),
         fallback: false,
