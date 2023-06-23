ca2b45009562fa6cef03d996ce5e450a52271328
Shiv Singh <shivsingh7150@hotmail.com>
2023-06-23 12:41:18 +0530

    Update [slug].js

--- pages/about/gallery/[slug].js
+++ pages/about/gallery/[slug].js
@@ -130,8 +130,6 @@ const Gallery = ({ galleries, categories, CurrentPage }) => {
 
                                 {PaginationData()}
 
-
-
                             </ul>
                         </div>
                     </section>
