0cbf17cf9d90823f040648bdb9de24b5f674355b
Shiv Singh <shivsingh7150@hotmail.com>
2023-08-10 17:54:31 +0530

    Update myform.js

--- pages/myform.js
+++ pages/myform.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   const body = req.body;
 
-  res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
+  //res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
 }
