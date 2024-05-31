863663fe030250044faf56d7d6a7020af3d63ec6
Shiv Singh <shivsingh7150@hotmail.com>
2024-05-31 10:58:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
     trailingSlash: true,
+    async headers() {
+        return [
+            {
+                source: '/:all*(svg|jpg|png)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=9999999999, must-revalidate',
+                    }
+                ],
+            },
+        ]
+    },
 }
