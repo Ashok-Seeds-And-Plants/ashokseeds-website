6c5800c00ac138893d493a4f3f2ac0c9621f3175
Shiv Singh <shivsingh7150@hotmail.com>
2024-06-11 17:11:14 +0530

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -111,7 +111,7 @@ const Blog = ({ post, categories }) => {
                                         <div className="post-author-info">
                                             <h4>Posted by<a href="#"> {username}</a></h4>
 
-                                            {parse(about)}
+                                            {about}
                                             <ul className="social-profile list-style style3">
                                                 <li>
                                                     <a href="https://facebook.com">
