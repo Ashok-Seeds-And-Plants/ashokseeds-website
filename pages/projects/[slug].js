ab2c13397f04c6fd2e8d92abea5921ce75313b91
Shiv Singh <shivsingh7150@hotmail.com>
2024-06-12 17:00:14 +0530

    Update [slug].js

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -5,9 +5,11 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Js from '@components/Js'
 
+import parse from 'html-react-parser';
+
 import { fetchAPI } from "../../lib/api"
 import delve from 'dlv'
-
+import { parseISO, format } from 'date-fns'
 import React from "react";
 import Link from "next/link";
 
@@ -64,7 +66,7 @@ const Gallery = ({ portfolios, categories, CurrentPage }) => {
         <>
             <Meta />
             <Head>
-                <title>Gallery Page - {CurrentPage} | Ashok Seeds and Plants</title>
+                <title>Projects Page - {CurrentPage} | Ashok Seeds and Plants</title>
                 <meta name="description" content="" />
 
             </Head>
@@ -76,22 +78,66 @@ const Gallery = ({ portfolios, categories, CurrentPage }) => {
                     <div className="breadcrumb-wrap bg-f br-1">
                         <div className="container">
                             <div className="breadcrumb-title">
-                                <h2>Gallery - {CurrentPage}</h2>
+                                <h2>Projects - {CurrentPage}</h2>
                                 <ul className="breadcrumb-menu list-style">
                                     <li><a href="https://www.ashokseedplant.com/">Home</a></li>
-                                    <li><a href="https://www.ashokseedplant.com/about/">About</a></li>
-                                    <li>Gallery</li>
+                                    <li>Projects</li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <section className="project-details-wrap ptb-100">
                         <div className="container">
-                        <div className="row justify-content-center">
-                                <h3>Loop will be here</h3>
+                            <div className="row justify-content-center">
+                                {portfolios.data.map((portfolio, i) => {
+
+                                    //console.log(post);
+                                    const title = delve(portfolio, "attributes.title");
+                                    const slug = delve(portfolio, "attributes.slug");
+                                    const cover = delve(portfolio, "attributes.cover.data.attributes.formats.medium.url");
+                                    const date = parseISO(delve(portfolio, "attributes.publishedAt"));
+                                    const excerpt = delve(portfolio, "attributes.excerpt");
+                                    const categories = delve(portfolio, "attributes.portfolio_categories.data");
+                                    // console.log(date);
+                                    return (
+                                        <div className="col-xl-4 col-lg-6 col-md-6">
+                                            <div className="project-card style1">
+                                                <div className="project-img">
+                                                    <img src={`${cover}`} alt={`${title}`} />
+                                                </div>
+                                                <div className="project-info">
+                                                    <img src="/img/shape-1.png" alt="Image" className="project-shape" />
+
+                                                    <h3><Link href={`/project/${slug}/`}>{title}</Link></h3>
+
+                                                    <ul className="categories-list">
+                                                        {categories.map((category, i) => {
+                                                            const cat_name = delve(category, "attributes.name");
+                                                            if (cat_name !== "All") {
+                                                                return (
+                                                                    <li>{cat_name}</li>
+
+                                                                )
+                                                            }
+                                                        })}
+
+                                                    </ul>
+
+                                                    {parse(excerpt)}
+
+                                                    <Link href={`/project/${slug}/`}>
+                                                        <a className="link style1">Read More <i
+                                                            className="flaticon-right-arrow"></i></a>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+
 
                             </div>
-                            
+
                             <ul className="page-nav list-style">
 
                                 {PaginationData()}
